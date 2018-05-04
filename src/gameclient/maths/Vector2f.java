751ed8ac56c959c370fdf036a717b3eace727e9d
RobinWebbers <38926211+RobinWebbers@users.noreply.github.com>
2018-05-04 14:38:38 +0200

    Add files via upload

---
+++ src/gameclient/maths/Vector2f.java
@@ -0,0 +1,15 @@
+package gameclient.maths;
+
+public class Vector2f {
+    
+    public float x,y;
+    
+    public Vector2f(){
+        
+    }
+    
+    public Vector2f(float x, float y){
+        this.x = x;
+        this.y = y;
+    }
+}
